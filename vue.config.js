b940f9859343fa6b2a9ef549dc8aa485b7052f11
halleflima <halleflima@hotmail.com>
2021-09-04 13:28:02 -0300

    vue config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/https://halleflima.github.io/BitCoin-Today"
-      : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "BitCoin-Today" : "/",
 };
